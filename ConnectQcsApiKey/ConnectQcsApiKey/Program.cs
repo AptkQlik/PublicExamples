d2a82b341dd59dd962b3962fec57c89b2b6c2be7
Øystein Kolsrud <yko@qlik.com>
2021-12-14 12:23:02 +0100

    Added example for connecting to QCS.

---
+++ ConnectQcsApiKey/ConnectQcsApiKey/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Qlik.Engine;
+
+namespace ConnectQcsApiKey
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            const string key = "<API key>";
+            const string appId = "<appId>";
+            const string url = "<uri>";
+
+            var location = QcsLocation.FromUri(url);
+            location.AsApiKey(key);
+
+            using (var app = location.App(appId))
+            {
+                Console.WriteLine(app.GetAppProperties().Title);
+            }
+        }
+	}
+}
