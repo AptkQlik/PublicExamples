9ab65386343d2409692f19a6b038c9c7675025ca
Øystein Kolsrud <yko@qlik.com>
2022-05-24 15:12:08 +0200

    Added examples illustrating how to detect and react to session timeouts.

---
+++ ObserveSessionTimeout/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using Qlik.Engine;
+using Qlik.Sense.JsonRpc;
+using Location = Qlik.Engine.Location;
+
+namespace ObserveSessionTimeout
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var url = "<url>";
+            var location = Location.FromUri(url);
+            location.AsNtlmUserViaProxy(certificateValidation: false);
+
+            using (var hub = location.Hub())
+            {
+                var sessionTimeoutEvent = new ManualResetEventSlim(false);
+                // In client managed versions of Qlik Sense, session timeouts are reported are reported through a push method.
+                hub.Session.PushMethodReceivedEvent += (o, a) => OnPushMethodReceivedEvent(o, a, sessionTimeoutEvent);
+                Console.WriteLine(DateTime.Now + " - Waiting for session timeout...");
+                sessionTimeoutEvent.Wait();
+                Console.WriteLine(DateTime.Now + " - Session timeout received.");
+            }
+        }
+
+        private static void OnPushMethodReceivedEvent(object sender, PushMethodReceivedEventArgs args, ManualResetEventSlim manualResetEventSlim)
+        {
+            Console.WriteLine(DateTime.Now + " - Push method received: " + args.Method);
+            if (args.Method == "OnSessionTimedOut")
+            {
+                manualResetEventSlim.Set();
+            }
+        }
+    }
+}
---
+++ ObserveSessionTimeoutQcs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using Qlik.Sense.JsonRpc;
+using Qlik.Sense.JsonRpc.WebSocket;
+using QcsLocation = Qlik.Engine.QcsLocation;
+
+
+namespace ObserveSessionTimeoutQcs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "<url>";
+            var key = "<api key>";
+            var location = QcsLocation.FromUri(url);
+            location.AsApiKey(key);
+
+            using (var app = location.SessionApp())
+            {
+                var sessionTimeoutEvent = new ManualResetEventSlim(false);
+                // In QCS versions of Qlik Sense, session timeouts are reported a websocket close status code.
+                app.Session.CommunicationErrorEvent += (o, a) => OnCommunicationErrorEvent(o, a, sessionTimeoutEvent);
+                Console.WriteLine(DateTime.Now + " - Waiting for session timeout...");
+                sessionTimeoutEvent.Wait();
+                Console.WriteLine(DateTime.Now + " - Session timeout received.");
+            }
+        }
+
+        private static void OnCommunicationErrorEvent(object sender, CommunicationErrorEventArgs args, ManualResetEventSlim manualResetEventSlim)
+        {
+            Console.WriteLine(DateTime.Now + " - Communication error detected: " + args.Exception.Message);
+            if (args.QlikWebSocketClosedStatus == QlikWebSocketClosedStatus.IdleTimeout)
+            {
+                manualResetEventSlim.Set();
+            }
+        }
+    }
+}
