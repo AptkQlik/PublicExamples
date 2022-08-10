2e3059bdbd012a150c3794646754b4aa07b3c0a6
Øystein Kolsrud <yko@qlik.com>
2022-08-10 13:14:17 +0200

    Added example: ConnectQcsJwt.

---
+++ ConnectQcsJwt/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Qlik.Engine;
+
+namespace ConnectQcsJwt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string url = "<uri>";
+            const string appId = "<appId>";
+            const string jwt = "<JSON Web Token>";
+
+            var location = QcsLocation.FromUri(url);
+            location.AsJwt(jwt);
+
+            using (var app = location.App(appId))
+            {
+                Console.WriteLine(app.GetAppProperties().Title);
+            }
+        }
+    }
+}
