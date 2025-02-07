368c34a942f8e4868dc2079f56ab4391fb9687d4
Øystein Kolsrud <yko@qlik.com>
2025-02-07 11:58:05 +0100

    Added example "ExportDataToExcel".

---
+++ ExportDataToExcel/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Qlik.Engine;
+using Qlik.Sense.RestClient;
+
+namespace ExportDataToExcel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "<url>";
+            var appId = "<appId>";
+            var objectId = "<objectId>";
+            var cubePath = "/qHyperCubeDef";
+            var destinationFile = @"ExportedData.xlsx";
+
+            var location = Location.FromUri(url);
+            location.AsNtlmUserViaProxy(certificateValidation: false);
+
+            ExportDataResult exportResult;
+            using (var app = location.App(appId))
+            {
+                var obj = app.GetGenericObject(objectId);
+                Console.WriteLine($"Exporting data from app: {appId}, object: {objectId}, hypercube path: \"{cubePath}\" ");
+                exportResult = obj.ExportData(NxExportFileType.EXPORT_OOXML, cubePath, serveOnce: true);
+            }
+
+            var client = new RestClient(url);
+            client.AsNtlmUserViaProxy(certificateValidation: false);
+            Console.WriteLine($"Exporting excel file to: {destinationFile}");
+            var binData = client.GetBytes(exportResult.Url);
+            File.WriteAllBytes(destinationFile, binData);
+            Console.WriteLine($"Wrote {binData.Length} to {destinationFile}.");
+        }
+    }
+}
